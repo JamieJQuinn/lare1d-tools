b3917f55713a11ad4678f866bdf89dda13c45b77
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-05-19 15:38:12 +0100

    Updated the energy plotting script to be... better

---
+++ print_energy.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python2
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import sys
+
+class Energy:
+    def __init__(self, fname, slice_vec=[], skip=0, nvars=None):
+        self.fromfile(fname, slice_vec, skip, nvars)
+
+    def read_data(self, f, slice_vec, en_nvars):
+        print "LOADING DATA"
+        self.data = np.fromfile(f, dtype=np.float64)
+        print "data size:", len(self.data)
+
+        n_timesteps = int(len(self.data)/en_nvars)
+        print "n_timesteps:", n_timesteps
+
+        offset = len(self.data) - n_timesteps*en_nvars
+        print "offset:", offset
+        if offset:
+            print "spare numbers:", self.data[-offset:]
+            self.data = self.data[:-offset]
+
+        self.data = self.data.reshape((n_timesteps, en_nvars))
+        if slice_vec:
+            print "Slice:", slice_vec
+            self.data = self.data[slice_vec[0] : slice_vec[1]]
+
+    def fromfile(self, fname, slice_vec=[], skip=0, nvars=0):
+        f = open(fname, 'rb')
+
+        if skip == 0:
+            print "LOADING HEADER"
+            self.magic         = f.read(3)
+            self.version       = self.read_int(f)
+            self.revision      = self.read_int(f)
+            self.endianness    = self.read_int(f)
+            self.header_length = self.read_int(f)
+            self.num_sz        = self.read_int(f)
+            self.en_nvars      = self.read_int(f)
+            self.id_length     = self.read_int(f)
+            self.varnames      = f.read(self.en_nvars*self.id_length).split()
+            print "version:", self.version
+            print "revision:", self.revision
+            print "endianness:", self.endianness
+            print "header_length:", self.header_length
+            print "num_sz:", self.num_sz
+            print "n_vars:", self.en_nvars
+            print "id_length:", self.id_length
+            self.read_data(f, slice_vec, self.en_nvars)
+        else:
+            f.read(skip)
+            self.read_data(f, slice_vec, nvars)
+
+        f.close()
+
+    def plot(self, ax, index):
+        ax.plot(en.data[:, 0], en.data[:,index], label=self.varnames[index])
+        ax.legend()
+
+    def fortran_to_int(self, n):
+        return int(n[::-1].encode('hex'), 16)
+
+    def read_int(self, fp):
+        integer_byte_size = 4
+        fortran_int = fp.read(integer_byte_size)
+        return self.fortran_to_int(fortran_int)
+
+    def print_max_at(self, t):
+        idx = (np.abs(self.data[:, 0]-t)).argmin()
+        print("{0} & {1} \\\\".format(format_number(self.data[idx, 8]), format_number(self.data[idx, 9])))
+
+def format_number(num):
+    num = float(num)
+    num = "{:.2e}".format(num)
+    mant, exp = num.split('e')
+    return "{0} \\times 10^{{{1}}}".format(mant, exp)
+
+parser = argparse.ArgumentParser(description='Print/plot lare3d en.dat files')
+
+parser.add_argument('filename', help='energy file to open')
+parser.add_argument('--print_latest', action='store_true',
+                    help='print latest row')
+parser.add_argument('--print_all', action='store_true',
+                    help='print all rows')
+parser.add_argument('--print_varnames', action='store_true',
+                    help='print saved variable names')
+parser.add_argument('--plot_columns', type=int, nargs='*',
+                    help='plot given columns')
+parser.add_argument('--slice_vec', type=int, nargs=2,
+                    help='take slice of data to plot')
+parser.add_argument('--nvars', type=int,
+                    help='Number of variables in en.dat')
+parser.add_argument('--bytes_to_skip', type=int, default=0)
+parser.add_argument('--xlim', type=float, nargs=2,
+                    help='limits of x-axis')
+parser.add_argument('--ylim', type=float, nargs=2,
+                    help='limits of y-axis')
+parser.add_argument('--plot_dt', action='store_true',
+                    help='plot dt')
+parser.add_argument('--plot_all', action='store_true',
+                    help='plot all variables')
+parser.add_argument('--debug', action='store_true',
+                    help='Enable debug mode')
+
+args = parser.parse_args()
+
+if args.debug:
+    print args
+
+en = Energy(args.filename, slice_vec=args.slice_vec, skip=args.bytes_to_skip, nvars=args.nvars)
+
+if args.print_varnames:
+    print en.varnames
+
+if args.print_latest:
+    print en.data[-1]
+
+if args.print_all:
+    for row in en.data:
+        np.set_printoptions(linewidth=140)
+        print row
+
+if args.plot_all:
+    args.plot_columns = range(1, en.en_nvars)
+
+if args.plot_dt:
+    time = en.data[:, 0]
+    dt = time[1:] - time[:-1]
+    plt.plot(dt)
+    plt.show()
+
+if args.plot_columns:
+    fig, axes = plt.subplots(len(args.plot_columns), sharex=True)
+    for axis, index in zip(axes, args.plot_columns):
+        en.plot(axis, index)
+        if args.ylim:
+            ax.set_ylim(args.ylim[0], args.ylim[1])
+        if args.xlim:
+            ax.set_xlim(args.xlim[0], args.xlim[1])
+    plt.tight_layout()
+    plt.show()
