6745e89c4a022d7dcba63e450a3763ca56cf0e64
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-06-11 14:05:00 +0100

    Updated euclid options & added better handling of binaries

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@ machine=''
 mpi_opts=''
 n_proc_arg=''
 output_dir=''
+lare_dir='.'
 clean=false
+exe='mpirun -np $n_proc $mpi_opts lare3d'
 
-while getopts "cn:m:o:i:" flag; do
+while getopts "n:m:o:i:" flag; do
   case "${flag}" in
     # Set number of processes to launch
     n) n_proc_arg="${OPTARG}" ;;
@@ -14,21 +16,24 @@ while getopts "cn:m:o:i:" flag; do
     # Output directory
     o) output_dir="${OPTARG}" ;;
     # lare3d folder location
-    i) cd "${OPTARG}" ;;
-    # Don't actually do anything if -c supplied
-    c) clean=true ;;
+    i) lare_dir="${OPTARG}" ;;
   esac
 done
 
 n_proc=''
 if [ "$machine" == "euclid" ]; then
-  n_proc=15
+  export PATH=/home/pgrad2/1101974q/prog/openmpi-euclid/open-mpi-build/bin:$PATH
+  export LD_LIBRARY_PATH=/home/pgrad2/1101974q/prog/openmpi-euclid/open-mpi-build/lib:$LD_LIBRARY_PATH
   mpi_opts+='--mca pml ob1'
 elif [ "$machine" == "euclid-torque" ]; then
   export PATH=/home/pgrad2/1101974q/prog/openmpi-euclid/open-mpi-build/bin:$PATH
   export LD_LIBRARY_PATH=/home/pgrad2/1101974q/prog/openmpi-euclid/open-mpi-build/lib:$LD_LIBRARY_PATH
   #mpi_opts+="--mca btl_base_verbose 30 --mca btl_tcp_if_exclude virbr0,lo"
   mpi_opts+="--mca btl_tcp_if_exclude virbr0,lo"
+elif [ "$machine" == "euclid-torque-intel" ]; then
+  . /opt/intel/compilers_and_libraries/linux/mpi/intel64/bin/mpivars.sh
+  n_proc=0 # Just need to define variable
+  exe='mpirun ./lare3d'
 elif [ "$machine" == "office" ]; then
   n_proc=4
 fi
@@ -37,17 +42,28 @@ if [ "$n_proc_arg" != '' ]; then
   n_proc=$n_proc_arg
 fi
 
-# Move binary
-cp bin/* Data
-
-# Run
-if [ "$clean" = false ]; then
-  time mpirun -np $n_proc $mpi_opts Data/lare3d
+if [ "$n_proc" == '' ]; then
+  echo "No processor number provided"
+  exit
 fi
 
-# Copy data
+data_dir=$(sed -n "s/data_dir = '\(.*\)'/\1/p" $lare_dir/bin/build_state)
+
+if [ "$data_dir" == '' ] || [ "$output_dir" == '' ]; then
+  echo "No output dir or data dir supplied"
+  exit
+fi
+
+# Run
 if [ "$output_dir" != '' ]; then
-  mkdir -p $output_dir
-  rm $output_dir/*
-  cp Data/* $output_dir
+  mkdir -p $output_dir/$data_dir
+  cp $lare_dir/bin/* $output_dir
+  cd $output_dir
+  time eval $exe
+  if [ "$data_dir" != '.' ]; then
+    mv $data_dir/* $output_dir
+    rmdir $data_dir
+  fi
+else
+  echo "No output directory given"
 fi
