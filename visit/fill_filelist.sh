2701ce9ba208077f1990805303c50bf3484cf0c5
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2018-02-06 11:23:49 +0000

    Added more visit scripts

--- visit/fill_filelist.sh
+++ visit/fill_filelist.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ls /nas/1101974q/kink_instability/mid-res/v-*/Data/*.sdf | sed -e 's/ /\\n/g' >> filename_list
+ls /nas/1101974q/kink-instability-str8/v-4r-4-isotropic-mid-res-B1/Data/*.sdf | sed -e 's/ /\\n/g' >> filename_list
--- visit/plot-ki-str8-side-slice.py
+++ visit/plot-ki-str8-side-slice.py
@@ -12,26 +12,6 @@ def save_fig(data_fname, fig_fname, varname):
 
   AddOperator("Slice", 0)
 
-  SliceAtts = SliceAttributes()
-  SliceAtts.originType = SliceAtts.Intercept  # Point, Intercept, Percent, Zone, Node
-  SliceAtts.originPoint = (0, 0, 0)
-  SliceAtts.originIntercept = 0
-  SliceAtts.originPercent = 0
-  SliceAtts.originZone = 0
-  SliceAtts.originNode = 0
-  SliceAtts.normal = (0, 0, 1)
-  SliceAtts.axisType = SliceAtts.ZAxis  # XAxis, YAxis, ZAxis, Arbitrary, ThetaPhi
-  SliceAtts.upAxis = (0, 1, 0)
-  SliceAtts.project2d = 1
-  SliceAtts.interactive = 1
-  SliceAtts.flip = 0
-  SliceAtts.originZoneDomain = 0
-  SliceAtts.originNodeDomain = 0
-  SliceAtts.meshName = "Grid/Grid"
-  SliceAtts.theta = 0
-  SliceAtts.phi = 90
-  SetOperatorOptions(SliceAtts, 0)
-
   DrawPlots()
 
   swatts = SaveWindowAttributes()
@@ -53,8 +33,6 @@ variables = [
   "Vmag",
   "Fluid/Rho",
   "Fluid/Energy",
-  "Fluid/Anisotropic_Viscous_Heating",
-  "Fluid/Isotropic_Viscous_Heating"
 ]
 
 for filename in filenames:
